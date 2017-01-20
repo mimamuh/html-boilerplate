aa54c5ed9f690c501c1755129c874f6247628fd3
Michael Neumair <hallo@michaelneumair.de>
2017-01-20 11:21:03 +0100

    [add] backgrounds addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,11 +2,12 @@
 * @Author: Matthias Gohla <Matze>
 * @Date:   2017-01-16T12:22:16+01:00
 * @Email:  matze_lebt@gmx.de
-* @Last modified by:   Matze
-* @Last modified time: 2017-01-16T12:24:17+01:00
+* @Last modified by:   mBook
+* @Last modified time: 2017-01-20T11:20:16+01:00
 */
 
 
 import '@kadira/storybook/addons';
 import '@kadira/storybook-addon-notes/register';
-import '@kadira/storybook-addon-knobs/register'
+import '@kadira/storybook-addon-knobs/register';
+import 'react-storybook-addon-backgrounds/register';
