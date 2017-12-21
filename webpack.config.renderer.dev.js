11b005c1ec975aed869021bd426a8a48a1152b3b
Michael Neumair <hallo@michaelneumair.de>
2017-12-21 16:01:35 +0100

    [add] browser-sync plugin

--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 	.BundleAnalyzerPlugin;
 const getPages = require('./webpack.config.pages').getPages;
@@ -121,6 +122,25 @@ module.exports = {
 			// Log level. Can be 'info', 'warn', 'error' or 'silent'.
 			logLevel: 'info',
 		}),
+
+		new BrowserSyncPlugin(
+			// BrowserSync options
+			{
+				// browse to http://localhost:3000/ during development
+				host: 'localhost',
+				port: 3000,
+				// proxy the Webpack Dev Server endpoint
+				// (which should be serving on http://localhost:8080/)
+				// through BrowserSync
+				proxy: 'http://localhost:8080/',
+			},
+			// plugin options
+			{
+				// prevent BrowserSync from reloading the page
+				// and let Webpack Dev Server take care of this
+				reload: false,
+			}
+		),
 	],
 
 	devServer: {
